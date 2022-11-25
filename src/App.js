2a03152c044527ace702fdb369ca6ee117fd3b0c
Leonard <MatheoJE_2000@hotmail.com>
2022-11-25 13:08:55 -0800

    Uh

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home, Login, Customers } from "./pages";
+import { Login } from "./pages";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/customers" element={<Customers />} />
+        <Route path="/" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const Login = () => {
-  return <></>;
+  return (
+    <div></div>
+  );
 };
 
 export default Login;
